08e97eb01a2aebbcd1c8e0a398dc59cc1a0b53ad
안종혁\ajh35 <ajh3533@gmail.com>
2024-03-12 23:49:54 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+  return (
+    <>
+      <Hello />
+      <div style={style}>{name}</div>
+    </>
+  );
+}
+
+export default App;
